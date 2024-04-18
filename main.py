5736802ed5eb5b21909aca2a51745a6fff0557de
dongshin <leened@naver.com>
2024-04-18 14:51:37 +0000

    init:main.py

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     """
     # TODO: Implement sum_of_squares_of_even
     pass
+    sum_squares = sum(num**2 for num in even_int_list)
+    return sum_squares
 
 # Main function
 def main():
